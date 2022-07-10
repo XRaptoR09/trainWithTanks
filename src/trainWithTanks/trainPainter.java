841f743cb16e1fff43c1726d96548ce29a122dcb
XRaptoR09 <Ivan.krasivskij09@gmail.com>
2022-07-10 18:00:58 +0300

    Empty train

--- src/trainWithTanks/trainPainter.java
+++ src/trainWithTanks/trainPainter.java
@@ -20,49 +20,194 @@ public class trainPainter extends Frame{
 		});
 	}
 	public void paint(Graphics g) {
-		//! Carriage for variables
-		//*Declarating variables
-		int mainY = 100, mainWidth = 240, mainHeight = 40;
-		int leftConnectX = 50, connectWidth = 6, connectHeight = 12, connectPaddingFromBottom = 15;
-		int wheelDiameter = 16, wheelPadding = 20, amountOfWheels = 3, amountOfPairs = 3;
+		//!Declarating variables
+		//*Locomotive variables
+		int locMainY = 100, locMainWidth = 120, locMainHeight = 40;
+		
+		int locCabinWidth = 35;
+		
+		int locLeftConnectX = 50, connectWidth = 11, connectHeight = 6, connectPaddingFromBottom = 3;
+		
+		int wheelDiameter = 16, wheelPadding = 20, amountOfBlocks = 2;
+		int wheelConnectorWidth = 10;
+		
+		//*Platform variables
+		int amountOfWheels = 2, amountOfPairs = 2;
 		//!Amount of colours should be the same as amountOfPairs
-		Color[] pairsColours = {Color.red, Color.green, Color.CYAN};
+		Color[] pairsColours = {Color.red, Color.CYAN};
+		
+		int platMainY = 125, platMainWidth = 120, platMainHeight = 15;
+		
+		int amountOfPlatforms = 3;
+
+		//*Carriage variables
+		int roofY = 100, roofWidth = 190, roofHeight = 5;
+		
+		int mainWidth = 150, mainHeight = 35;
+		int amountOfWindows = 4, windowHeight = 15;
+		
+		int spaceAround = mainWidth / ((amountOfWindows * 2) + 1);
+
+		int windowWidth = 15, windowPadding = spaceAround;
+
+		int amountOfCarriages = 2;
 
 		//*Countable variables
+		//*Locomotive
 		//Добавляем смещение из-за начала координат вне Фрейма
-		mainY = mainY + 30;
-		leftConnectX = leftConnectX + 8;
+		locMainY = locMainY + 30;
+		locLeftConnectX = locLeftConnectX + 8;
 
-		int	mainX = (leftConnectX + connectWidth),
-		
-		connectY = (((mainY + mainHeight) - connectPaddingFromBottom) - connectHeight),
-		rightConnectX = (mainX + mainWidth),
-		
-		wheelsY = (mainY + mainHeight),
-		wheelX = (mainX + wheelPadding),
+		int	
+		locCabinX = (locLeftConnectX + connectWidth),
+		locCabinHeight = (locMainHeight / 3),
+		locCabinY = (locMainY + locMainHeight) - locCabinHeight,
 
-		pairWidth = wheelDiameter * amountOfWheels,
-		pairShift = ((mainWidth) - (((wheelDiameter * amountOfWheels) * amountOfPairs) + (wheelPadding * 2)));
-		if (amountOfPairs > 2) {
-			pairShift = pairShift / (amountOfPairs - 1);
+		locMainX = (locCabinX + locCabinWidth),
+		
+		connectY = (((locMainY + locMainHeight) - connectPaddingFromBottom) - connectHeight),
+		locRightConnectX = (locMainX + locMainWidth),
+		
+		wheelsY = (locMainY + locMainHeight),
+		locWheelX = (locCabinX + wheelPadding),
+		wheelConnectorX = locWheelX + wheelDiameter,
+		wheelConnectorY = wheelsY + (wheelDiameter / 3),
+		wheelConnectorHeight = (wheelDiameter / 3),
+
+		glassX1 = locCabinX,
+		glassY1 = locCabinY,
+		glassX2 = locMainX,
+		glassY2 = locMainY,
+
+
+		blockShift = ((locCabinWidth + locMainWidth) - (((wheelDiameter * 2) * 2) + (wheelPadding * 2) + (wheelConnectorWidth * 2)));
+		if (amountOfBlocks > 2) {
+			blockShift = blockShift / (amountOfBlocks - 1);
 		}
 
-		//!Code
-		if (pairsColours.length != amountOfPairs) {
-			g.setColor(Color.red.brighter());
-			g.setFont(new Font("Arial", Font.BOLD, 50));
-			g.drawString("Mistake in your code!!!".toUpperCase(), 30, 100);
-		}else{
-			g.drawRect(mainX, mainY, mainWidth, mainHeight);						//main
-			g.drawRect(leftConnectX, connectY, connectWidth, connectHeight);			//connect-left
-			g.drawRect(rightConnectX, connectY, connectWidth, connectHeight);			//connect-right
-			for (int i = 0; i < amountOfPairs; i++) {
-				g.setColor(pairsColours[i]);
-				for (int j = 0; j < amountOfWheels; j++) {
-					g.drawOval(wheelX, wheelsY, wheelDiameter, wheelDiameter);		//2 wheels
-					wheelX = wheelX + wheelDiameter;
+		
+
+		//! Locomotive
+			g.drawRect(locMainX, locMainY, locMainWidth, locMainHeight);			//main
+			g.drawRect(locCabinX, locCabinY, locCabinWidth, locCabinHeight);		//cabin
+			g.drawRect(locLeftConnectX, connectY, connectWidth, connectHeight);		//connect-left
+			g.drawRect(locRightConnectX, connectY, connectWidth, connectHeight);	//connect-right
+			g.drawLine(glassX1, glassY1, glassX2, glassY2);		//glass
+			for (int i = 0; i < amountOfBlocks; i++) {
+				for (int j = 0; j < 2; j++) {
+					g.drawOval(locWheelX, wheelsY, wheelDiameter, wheelDiameter);		//2 wheels
+					locWheelX = locWheelX + wheelConnectorWidth + wheelDiameter;
 				}
-				wheelX = wheelX + pairShift;
+				g.drawRect(wheelConnectorX, wheelConnectorY, wheelConnectorWidth, wheelConnectorHeight);
+				wheelConnectorX = (wheelConnectorX + blockShift) + ((wheelDiameter * 2) + wheelConnectorWidth);
+				locWheelX = locWheelX - wheelConnectorWidth;
+				locWheelX = locWheelX + blockShift;
+			}
+
+		//!Platform
+		int platLeftConnectX = (locRightConnectX + connectWidth);
+
+		platMainY = platMainY + 30;
+
+		int	platMainX = (platLeftConnectX + connectWidth),
+		
+		platRightConnectX = (platMainX + platMainWidth);
+		int
+		wheelX = (platMainX + wheelPadding),
+
+		platformWidth = (connectWidth * 2) + platMainWidth,
+
+		platPairShift = ((platMainWidth) - (((wheelDiameter * amountOfWheels) * amountOfPairs) + (wheelPadding * 2)));
+		
+		//*Paint
+		for (int p = 0; p < amountOfPlatforms; p++) {
+		if (amountOfPairs > 2) {
+			platPairShift = platPairShift / (amountOfPairs - 1);
+		}
+			if (pairsColours.length != amountOfPairs) {
+				g.setColor(Color.red.brighter());
+				g.setFont(new Font("Arial", Font.BOLD, 50));
+				g.drawString("Mistake in your code!!!".toUpperCase(), 30, 100);
+			}else{
+				g.setColor(Color.black);
+				g.drawRect(platLeftConnectX, connectY, connectWidth, connectHeight);		//connect-left
+				g.drawRect(platMainX, platMainY, platMainWidth, platMainHeight);			//main
+				g.drawRect(platRightConnectX, connectY, connectWidth, connectHeight);		//connect-right
+				for (int i = 0; i < amountOfPairs; i++) {
+					g.setColor(pairsColours[i]);
+					for (int j = 0; j < amountOfWheels; j++) {
+						g.drawOval(wheelX, wheelsY, wheelDiameter, wheelDiameter);		//2 wheels
+						wheelX = wheelX + wheelDiameter;
+					}
+					wheelX = wheelX + platPairShift;
+				}
+				platLeftConnectX = platLeftConnectX + (platformWidth);
+				platMainX = (platLeftConnectX + connectWidth);
+				platRightConnectX = (platMainX + platMainWidth);
+				wheelX = (platMainX + wheelPadding);
+			}
+		}
+		platLeftConnectX = platLeftConnectX - (platformWidth);
+		platMainX = (platLeftConnectX + connectWidth);
+		platRightConnectX = (platMainX + platMainWidth);
+		wheelX = (platMainX + wheelPadding);
+
+		//! Carriage for variables
+		roofY = roofY + 30;
+		//*Countable variables
+		int leftConnectX = (platRightConnectX + connectWidth);
+		
+		int	mainY = (roofY + roofHeight),
+		mainX = (leftConnectX + connectWidth),
+		
+		roofX = (mainX - ((roofWidth - mainWidth) / 2)),
+		
+		rightConnectX = (mainX + mainWidth),
+		
+		wheel1X = (mainX + wheelPadding),
+		
+		windowShift = (mainWidth - (windowPadding * 2)) / ((amountOfWindows * 2) - 1),
+		windowsY = (mainY + (windowHeight / 2)),
+		windowX = (mainX + windowPadding);
+		
+		if (windowWidth != windowPadding){
+			windowX = (mainX + windowPadding) + ((windowPadding - windowWidth));
+		}
+
+		int pairShift = ((mainWidth) - (((wheelDiameter * amountOfWheels) * amountOfPairs) + (wheelPadding * 2)));
+		
+		
+		//!Code
+		for (int i = 0; i < amountOfCarriages; i++) {
+			if (roofWidth < mainWidth) {
+				System.out.println("Roof is too short!!!");
+			}else{
+				for (int p = 0; p < amountOfPairs; p++) {
+					g.setColor(pairsColours[i]);
+					for (int j = 0; j < amountOfWheels; j++) {
+						g.drawOval(wheel1X, wheelsY, wheelDiameter, wheelDiameter);		//2 wheels
+						wheel1X = wheel1X + wheelDiameter;
+					}
+					wheel1X = wheel1X + pairShift;
+				}
+				g.setColor(Color.black);
+				g.drawRect(mainX, mainY, mainWidth, mainHeight);				//main
+				g.drawRect(roofX, roofY, roofWidth, roofHeight);				//roof
+				g.drawRect(leftConnectX, connectY, connectWidth, connectHeight);	//connect-left
+				g.drawRect(rightConnectX, connectY, connectWidth, connectHeight);	//connect-right
+				for(int w = 0; w < amountOfWindows; w++) {									//windows
+					g.drawRect(windowX, windowsY, windowWidth, windowHeight);		
+					windowX = windowX + (spaceAround * 2);
+				}
+			}
+			leftConnectX = leftConnectX + ((connectWidth * 2) + mainWidth);
+			mainX = (leftConnectX + connectWidth);
+			roofX = (mainX - ((roofWidth - mainWidth) / 2));
+			rightConnectX = (mainX + mainWidth);
+			wheel1X = (mainX + wheelPadding);
+			windowX = (mainX + windowPadding);
+			if (windowWidth != windowPadding){
+				windowX = (mainX + windowPadding) + ((windowPadding - windowWidth));
 			}
 		}
 	}
