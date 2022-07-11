d2a7e735cffa7009f575642a4d7254dc0268eb08
XRaptoR09 <Ivan.krasivskij09@gmail.com>
2022-07-11 15:16:38 +0300

    train with tanks

--- src/trainWithTanks/trainPainter.java
+++ src/trainWithTanks/trainPainter.java
@@ -32,30 +32,41 @@ public class trainPainter extends Frame{
 		
 		//*Platform variables
 		int amountOfWheels = 2, amountOfPairs = 2;
-		//!Amount of colours should be the same as amountOfPairs
-		Color[] pairsColours = {Color.red, Color.CYAN};
+
 		
 		int platMainY = 125, platMainWidth = 120, platMainHeight = 15;
 		
-		int amountOfPlatforms = 3;
+		int amountOfPlatforms = 2;
+
+		//*Tank variables
+
+		int bodyWidth = 80;
+
+		int dullDiameter = 2;
+
+		int flagstockWidth = 2;
+
+		int amountOfColors = 2;
+
+		int caterpillarPadding = 4;
+		
 
 		//*Carriage variables
 		int roofY = 100, roofHeight = 5;
 		
 		int mainHeight = 35;
-		int amountOfWindows = 7, windowHeight = 15;
 		
+		int amountOfWindows = 4, windowHeight = 15;
 		int windowWidth = 15;
-
 		int windowPadding = 20;
 		
-		int amountOfCarriages = 2;
+		int amountOfCarriages = 0;
 		
 		//*Countable variables
 		int mainWidth = ((windowWidth * amountOfWindows) + (windowPadding * (amountOfWindows + 1)));
-		int spaceAround = (mainWidth - (windowWidth * amountOfWindows)) / ((amountOfWindows * 2) + 1);
 		
 		int roofWidth = mainWidth + 20; 
+
 		//*Locomotive
 		//Добавляем смещение из-за начала координат вне Фрейма
 		locMainY = locMainY + 30;
@@ -122,22 +133,17 @@ public class trainPainter extends Frame{
 
 		platPairShift = ((platMainWidth) - (((wheelDiameter * amountOfWheels) * amountOfPairs) + (wheelPadding * 2)));
 		
-		//*Paint
 		for (int p = 0; p < amountOfPlatforms; p++) {
 		if (amountOfPairs > 2) {
 			platPairShift = platPairShift / (amountOfPairs - 1);
 		}
-			if (pairsColours.length != amountOfPairs) {
-				g.setColor(Color.red.brighter());
-				g.setFont(new Font("Arial", Font.BOLD, 50));
-				g.drawString("Mistake in your code!!!".toUpperCase(), 30, 100);
-			}else{
+			
 				g.setColor(Color.black);
 				g.drawRect(platLeftConnectX, connectY, connectWidth, connectHeight);		//connect-left
 				g.drawRect(platMainX, platMainY, platMainWidth, platMainHeight);			//main
 				g.drawRect(platRightConnectX, connectY, connectWidth, connectHeight);		//connect-right
 				for (int i = 0; i < amountOfPairs; i++) {
-					g.setColor(pairsColours[i]);
+					
 					for (int j = 0; j < amountOfWheels; j++) {
 						g.drawOval(wheelX, wheelsY, wheelDiameter, wheelDiameter);		//2 wheels
 						wheelX = wheelX + wheelDiameter;
@@ -148,14 +154,108 @@ public class trainPainter extends Frame{
 				platMainX = (platLeftConnectX + connectWidth);
 				platRightConnectX = (platMainX + platMainWidth);
 				wheelX = (platMainX + wheelPadding);
-			}
+			
+				//!Tank
+				//*Counting variables
+				
+				int bodyX = (platMainX + ((platMainWidth - bodyWidth) / 2)) - platformWidth;
+				
+				
+				int towerWidth =  bodyWidth / 3;
+				towerWidth = (towerWidth + (towerWidth / 5));
+				
+				int towerHeight = (towerWidth / 2),
+				towerX = (bodyX + towerWidth) - (towerWidth / 5);
+
+				
+				int
+				bodyHeight = bodyWidth / 4;
+				
+				int towerY = (platMainY - ((towerHeight * 2) + bodyHeight));
+
+				int bodyY = towerY + towerHeight,
+				
+				dullLength = bodyWidth / 2,
+				dullX = towerX + towerWidth,
+				dullY = towerY + ((towerHeight - dullDiameter) / 2),
+				
+				flagstockX = towerX - ((towerWidth / 5) + flagstockWidth),
+				flagstockY = towerY,
+				flagstockHeight = towerHeight,
+		
+				flagHeight = (flagstockHeight / 3) * 2,
+				flagWidth = flagstockHeight,
+				
+				tankWheelDiameter = towerHeight,
+				tankWheelsY = bodyY + bodyHeight,
+				tankWheelXStart = (bodyX + caterpillarPadding) + ((bodyWidth - ((tankWheelDiameter *4) + (caterpillarPadding * 2))) / 2),
+				tankWheelX = tankWheelXStart;
+				
+				for (int c = 0; c < 4; c++) {
+					tankWheelX = tankWheelX + tankWheelDiameter;
+				}
+				int tankWheelXEnd = tankWheelX;
+		
+				int
+				leftLineX1 = bodyX,
+				rightLineX1 = bodyX + bodyWidth,
+				linesBetweenY1 = tankWheelsY,
+				leftLineX2 = tankWheelXStart + (tankWheelDiameter / 3),
+				rightLineX2 = tankWheelXEnd - (tankWheelDiameter / 4),
+				linesBetweenY2 = tankWheelsY + tankWheelDiameter,
+				
+				lowLineX1 = tankWheelXStart + (tankWheelDiameter / 2),
+				lowLineY1 = linesBetweenY2,
+				lowLineX2 = tankWheelXEnd - (tankWheelDiameter / 2),
+				lowLineY2 = lowLineY1;
+		
+				//*Flag
+				int flagXStart = (flagstockX - flagWidth);
+				int colorHeight = (flagHeight / amountOfColors);
+				if (flagHeight % amountOfColors != 0) {
+					flagHeight = flagHeight + (amountOfColors - (flagHeight % amountOfColors));
+				}
+				colorHeight = (flagHeight / amountOfColors);
+		
+				g.setColor(Color.blue);
+				g.fillRect(flagXStart, flagstockY, flagWidth, colorHeight);
+				g.setColor(Color.yellow);
+				g.fillRect(flagXStart, flagstockY + colorHeight, flagWidth, colorHeight);
+				g.setColor(Color.lightGray);
+				g.fillRect(flagstockX, flagstockY, flagstockWidth, flagstockHeight);
+				
+				g.setColor(Color.black);
+				g.drawRect(flagXStart, flagstockY, flagWidth, flagHeight);
+				g.drawRect(flagstockX, flagstockY, flagstockWidth, flagstockHeight);
+				g.drawRect(towerX, towerY, towerWidth, towerHeight);
+				g.drawRect(dullX, dullY, dullLength, dullDiameter);
+				g.drawRect(bodyX, bodyY, bodyWidth, bodyHeight);
+				
+				g.drawLine(leftLineX1, linesBetweenY1, leftLineX2, linesBetweenY2);
+				g.drawLine(rightLineX1, linesBetweenY1, rightLineX2, linesBetweenY2);
+				g.drawLine(lowLineX1, lowLineY1, lowLineX2, lowLineY2);
+		
+				tankWheelX = tankWheelXStart;
+				g.setColor(Color.white);
+				for (int tb = 0; tb < 4; tb++) {
+					g.fillOval(tankWheelX, tankWheelsY, tankWheelDiameter, tankWheelDiameter);
+					tankWheelX = tankWheelX + tankWheelDiameter;
+				}
+				g.setColor(Color.black);
+		
+				tankWheelX = tankWheelXStart;
+				for (int t = 0; t < 4; t++) {
+					g.drawOval(tankWheelX, tankWheelsY, tankWheelDiameter, tankWheelDiameter);
+					tankWheelX = tankWheelX + tankWheelDiameter;
+				}
 		}
 		platLeftConnectX = platLeftConnectX - (platformWidth);
 		platMainX = (platLeftConnectX + connectWidth);
 		platRightConnectX = (platMainX + platMainWidth);
 		wheelX = (platMainX + wheelPadding);
 
-		//! Carriage for variables
+
+		//! Carriage 
 		roofY = roofY + 30;
 		//*Countable variables
 		int leftConnectX = (platRightConnectX + connectWidth);
@@ -195,7 +295,7 @@ public class trainPainter extends Frame{
 				pairShift = ((mainWidth) - (((wheelDiameter * amountOfWheels) * amountOfPairs) + (wheelPadding * 2)));
 
 				for (int p = 0; p < amountOfPairs; p++) {
-					g.setColor(pairsColours[i]);
+					
 					for (int j = 0; j < amountOfWheels; j++) {
 						g.drawOval(wheel1X, wheelsY, wheelDiameter, wheelDiameter);		//2 wheels
 						wheel1X = wheel1X + wheelDiameter;
